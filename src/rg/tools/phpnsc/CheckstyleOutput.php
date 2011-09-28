54303afcbb687906f8ece37b9bccf4bb3482b6e0
Bastian Hofmann <bashofmann@googlemail.com>
2011-09-28 15:01:25 +0200

    finalized license information

--- src/rg/tools/phpnsc/CheckstyleOutput.php
+++ src/rg/tools/phpnsc/CheckstyleOutput.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of phpnsc.
  *
- * (c) Bastian Hofmann <bastian.hofmann@researchgate.net>
+ * (c) ResearchGate GmbH <bastian.hofmann@researchgate.net>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
